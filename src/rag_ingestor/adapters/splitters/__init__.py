5fb02569494ff0fb754bb973b4631df816bb78f1
bendrummond389 <60265150+bendrummond389@users.noreply.github.com>
2025-04-06 04:49:30 -0600

    rewrite

--- src/rag_ingestor/adapters/splitters/__init__.py
+++
@@ -1,9 +0,0 @@
-from .markdown_splitter import MarkdownSplitter
-from .plain_splitter import PlainTextSplitter
-from .splitter_manager import SplitterManager
-
-__all__ = [
-    "PlainTextSplitter",
-    "MarkdownSplitter",
-    "SplitterManager",
-]
